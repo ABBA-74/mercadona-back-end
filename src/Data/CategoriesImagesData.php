74b8a0ea4b46bac6e317969a0fa15d0acffa066c
Abdel Bailoul <ab.bailoul@gmail.com>
2023-09-13 02:36:34 +0200

    feat: created image fixtures

---
+++ src/Data/CategoriesImagesData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Data;
+
+class CategoriesImagesData
+{
+    public const CATEGORIES = [
+        'aperitif' => 'aperitif.png',
+        'boucherie' => 'boucherie.png',
+        'dessert' => 'dessert.png',
+        'fromages' => 'fromages.png',
+        'fruits_et_legumes_bio' => 'fruits-legumes-bio.png',
+        'fruits' => 'fruits.png',
+        'glaces' => 'glaces.png',
+        'jus_de_fruits_frais' => 'jus-fruits-frais.png',
+        'legumes' => 'legumes.png',
+        'pates_et_riz' => 'pates-riz.png',
+        'poissons_et_crustaces' => 'poissons-crustaces.png',
+        'volaille_et_lapin' => 'volaille-lapin.png',
+    ];
+}
