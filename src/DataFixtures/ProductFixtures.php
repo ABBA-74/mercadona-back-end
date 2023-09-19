e768b3add01dbd4cf997598c304cf6015d020f65
Abdel Bailoul <ab.bailoul@gmail.com>
2023-09-19 09:22:51 +0200

    feat: renamed setPrice to setOriginalPrice

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -23,7 +23,7 @@ class ProductFixtures extends Fixture implements DependentFixtureInterface
 
             $product->setLabel($productLabel)
                 ->setDescription($productDescription)
-                ->setPrice($faker->randomFloat(2, 3, 8))
+                ->setOriginalPrice($faker->randomFloat(2, 3, 8))
                 ->setCreatedAt($faker->dateTimeBetween('-5 week', '-1 week'))
                 ->setImage($this->getReference('productImage_' . $i))
                 ->setUser($this->getReference('admin_' . $faker->numberBetween(1, 10)))
