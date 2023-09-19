ca9b016a6db49aeb91f2351d701853f1f8d1541a
Abdel Bailoul <ab.bailoul@gmail.com>
2023-09-19 09:27:58 +0200

    feat: added findBestPromotion method to promotion repository

--- src/Repository/PromotionRepository.php
+++ src/Repository/PromotionRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Promotion;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Collections\Collection;
 
 /**
  * @extends ServiceEntityRepository<Promotion>
@@ -21,28 +22,22 @@ class PromotionRepository extends ServiceEntityRepository
         parent::__construct($registry, Promotion::class);
     }
 
-//    /**
-//     * @return Promotion[] Returns an array of Promotion objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findBestPromotion(?Collection $promotions): ?Promotion
+    {
+        $maxDiscountPercentage = 0;
+        $bestPromotion = null;
+        $now = new \DateTime();
 
-//    public function findOneBySomeField($value): ?Promotion
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        foreach ($promotions as $promotion) {
+            if ($promotion->getStartDate() <= $now && $promotion->getEndDate() >= $now) {
+                $discountPercentage = $promotion->getDiscountPercentage();
+
+                if ($discountPercentage > $maxDiscountPercentage) {
+                    $maxDiscountPercentage = $discountPercentage;
+                    $bestPromotion = $promotion;
+                }
+            }
+        }
+        return $bestPromotion;
+    }
 }
