9e948825fb36e1a29b8a199f1a7ab1d54fc193a0
Abdel Bailoul <ab.bailoul@gmail.com>
2023-09-20 12:13:08 +0200

    feat: updated ProductRepository methods

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -16,15 +16,43 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class ProductRepository extends ServiceEntityRepository
 {
+    private $em;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+        $this->em =  $this->getEntityManager();
     }
 
+    /**
+    * Saves a product by persisting it to the database.
+    *
+    * @param Product $product The product to save.
+    */
     public function save(Product $product): void
     {
-        $entityManager = $this->getEntityManager();
-        $entityManager->persist($product);
-        $entityManager->flush();
+        $this->em->persist($product);
+        $this->em->flush();
+    }
+
+    /**
+    * Updates the discount prices of a product.
+    *
+    * @param Product $product The product to update.
+    * @param int|null $discountPercentage The discount percentage to apply (can be null).
+    * @param int|null $discountedPrice The reduced price (can be null).
+    */
+    public function updateDiscountPrice(
+        Product $product,
+        ?float $discountPercentage,
+        ?float $discountedPrice
+    ): void
+    {
+        // Update the product's discount properties
+        $product->setDiscountedPrice($discountedPrice);
+        $product->setCurrentPromotionPercentage($discountPercentage);
+
+        // Flush to persist the changes to the database
+        $this->em->flush();
     }
 }
